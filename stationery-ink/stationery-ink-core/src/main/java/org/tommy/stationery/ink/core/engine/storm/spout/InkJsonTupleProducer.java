169d9d620004ef78da4b416f6a44b45c97634883
tommy.hilfiger <tommy.hilfiger@daumkakao.com>
2016-03-14 15:47:26 +0900

    #feature : jms(activemq) spout supported.

--- stationery-ink/stationery-ink-core/src/main/java/org/tommy/stationery/ink/core/engine/storm/spout/InkJsonTupleProducer.java
+++ stationery-ink/stationery-ink-core/src/main/java/org/tommy/stationery/ink/core/engine/storm/spout/InkJsonTupleProducer.java
@@ -4,7 +4,11 @@ import backtype.storm.contrib.jms.JmsTupleProducer;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Values;
+import org.apache.activemq.command.ActiveMQBytesMessage;
+import org.apache.activemq.util.ByteSequence;
+import org.apache.hadoop.hbase.util.Bytes;
 
+import javax.jms.BytesMessage;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.TextMessage;
@@ -17,6 +21,15 @@ public class InkJsonTupleProducer implements JmsTupleProducer {
     public Values toTuple(Message msg) throws JMSException {
         if(msg instanceof TextMessage){
             String json = ((TextMessage) msg).getText();
+            return new Values(json);
+        } else if(msg instanceof ActiveMQBytesMessage) {
+            String json = "";
+            ActiveMQBytesMessage bytesMessage = ((ActiveMQBytesMessage)msg);
+            if (bytesMessage != null){
+                byte []bt = new byte[(int) bytesMessage.getBodyLength()];
+                bytesMessage.readBytes(bt);
+                json = new String(bt);
+            }
             return new Values(json);
         } else {
             return new Values((msg != null ? msg.toString() : ""));
